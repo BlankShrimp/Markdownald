d709383ed23e9414f4279a3c884fbcbbcec71923
BlankShrimp <vanishing.19@gmail.com>
2019-04-30 18:51:46 +0800

    add autocompletion for table

--- src/editor.js
+++ src/editor.js
@@ -3,20 +3,53 @@ const $input = $('#input');
 const rendered = $('#main')[0];
 const showdown = require('showdown');
 
+//create editor object
 var editor = CodeMirror.fromTextArea($input[0], {
     mode: "markdown",
     lineNumbers: true,
     theme: "monokai",
     cursorHeight: 0.85,
-    lineWrapping:true
+    lineWrapping:true,
+    extraKeys: {
+        //auto complete: table
+        Enter: function() {
+            var flag1 = true;
+            var flag2 = true;
+            var thisLine = editor.getLine(editor.getCursor().line);
+            if (thisLine.startsWith("|") && thisLine.endsWith("|")) {
+                var count = thisLine.split("|").length;
+                if (editor.getCursor().line != editor.lastLine()) {
+                    flag1 = false;
+                    var nextLine = editor.getLine(editor.getCursor().line + 1).split("|");
+                    for (var i= 0; i < nextLine.length; i++) {
+                        var period = nextLine[i];
+                        if (period.search("-----") > -1) {
+                            flag2 = false
+                            break;
+                        }
+                    }
+                }
+                if (flag1 || flag2) {
+                    tableSeparator = "|"
+                    for (let i = 2; i < count; i++) {
+                        tableSeparator += "-----|";
+                    }
+                    editor.replaceSelection("\r\n" + tableSeparator);
+                }
+            }
+            editor.replaceSelection("\r\n");
+        }
+    }
 });
 
+//create rendered object
 var converter = new showdown.Converter({
     tables: true,
     strikethrough: true,
     tasklists: true
 });
 
+//call render when sth changed in editor
 editor.on("change", function(editor, change) {
     text = editor.getValue();
     html = converter.makeHtml(text);
@@ -24,6 +57,7 @@ editor.on("change", function(editor, change) {
     insertPosition.innerHTML = html;
 })
 
+//set simultaneous scroll
 var timer = null;
 editor.on("scroll", function(editor) {
     rendered.removeAttribute("onScroll");
@@ -35,9 +69,7 @@ editor.on("scroll", function(editor) {
         rendered.setAttribute("onScroll", 'scrollRendered();');
     }, 300)
 })
-
 rendered.setAttribute("onScroll", "scrollRendered();");
-
 function scrollRendered() {
     var scale = (editor.getScrollerElement().scrollHeight - editor.getScrollerElement().clientHeight) 
     / (rendered.scrollHeight - rendered.clientHeight);
@@ -45,4 +77,6 @@ function scrollRendered() {
     timer = setTimeout(function() {
         editor.getScrollerElement().scrollTop = (rendered.scrollTop * scale);
     }, 50)
-}
+};
+
+//auto complete: table
