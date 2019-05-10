7232b6f34da4f624e93d380fab4dda5a6375cedd
Takumichan <37031501+Taku-firi@users.noreply.github.com>
2019-05-11 01:07:19 +0800

    folder structure added, table improved

--- src/dbhelper/sql.js
+++ src/dbhelper/sql.js
@@ -64,7 +64,7 @@ let db = new HandleDB({
 });
 
 db.connectDataBase().then((result)=>{
-    console.log(result);
+
     // create(load) table
 
     let supportTable =`
@@ -76,7 +76,7 @@ db.connectDataBase().then((result)=>{
 
     let personTable =`
         create table if not exists Persons (
-            PersonId int(3) PRIMARY KEY,
+            PersonId varchar(255) PRIMARY KEY,
             Name varchar(255) NOT NULL,
             Password varchar(255) NOT NULL
         );`;
@@ -84,13 +84,11 @@ db.connectDataBase().then((result)=>{
     
     let directoryTable =`
         create table if not exists Directories (
-            id int(3) PRIMARY KEY,
-            Value varchar(255) NOT NULL,
-            NoteId int(3) UNIQUE,
+            FolderId int(3) PRIMARY KEY,
+            FolderName varchar(255) NOT NULL,
             ParentId int(3) NOT NULL,
             key varchar(255) NOT NULL,
-            level int(3) NOT NULL,
-            FOREIGN KEY(NoteId) REFERENCES Notes(NoteId) on delete cascade on update cascade
+            level int(3) NOT NULL
         );`;
         db.createTable(directoryTable);
 
@@ -99,15 +97,14 @@ db.connectDataBase().then((result)=>{
             NoteId int(3) PRIMARY KEY, 
             Title varchar(255),
             data MEDIUMTEXT,
-            Datetime DATETIME
+            FolderId int(3),
+            ModifyTime DATETIME,
+            ViewTime DATETIME
         );`;
     return db.createTable(noteTable);
 
 }).then((result)=>{
-    console.log(result);
-
     doSql();
-
 }).catch((err)=>{
     console.error(err);
 });
@@ -115,71 +112,66 @@ db.connectDataBase().then((result)=>{
 let doSql = function() {
     addSupport(["MarkdownTutorial","Content1"]);
     addSupport(["ServerGuide","Content2"]);
-    // addPerson([1,"hdk1","123456"]);
-    // addPerson([2,"hdk2","1234567"]);
-    // addPerson([3,"hdk3","12345678"]);
-    // addNote([1,"title1","data1"],[1,"gf1",1,0,"0",1]);
-    // addNote([2,"title2","data2"],[2,"gf2",2,0,"0",1]);
-    // addNote([3,"title3","data3"],[3,"f1_1",3,1,"0-1",2]);
-    // addNote([4,"title4","data4"],[4,"f1_2",4,1,"0-1",2]);
-    // addNote([5,"title5","data5"],[5,"f2_1",5,2,"0-2",2]);
-    // addNote([6,"title6","data6"],[6,"s1_1_1",6,3,"0-1-3",3]);
-    // addNote([7,"title7","data7"],[7,"s1_1_2",7,3,"0-1-3",3]);
-    // deletePerson([3]);
+    // addPerson(["donk","hdk1","123456"]);
+    // addPerson(["miyaaa","lmy","1234567"]);
+    // addPerson(["fordai","dzj","12345678"]);
+    // addNote([1,"title1","data1",1],[1,"Year1",0,"0",1]);
+    // addNote([2,"title2","data2",2],[2,"Year2",0,"0",1]);
+    // addNote([3,"title3","data3",3],[3,"S1",1,"0-1",2]);
+    // addNote([4,"title4","data4",3],[3,"S1_",1,"0-1",2]);
+    // addNote([5,"title5","data5",4],[4,"S2",2,"0-2",2]);
+    // addNote([6,"title6","data6",5],[5,"EAP",3,"0-1-3",3]);
+    // addNote([7,"title7","data7",6],[6,"CCT",3,"0-1-3",3]);
+    // addNote([8,"title8","data8",7],[7,"S1",2,"0-2",2]);
+    // deletePerson(["fordai"]);
     // deleteNote([2]);
     // updateNoteTitle(["title1mod",1]);
-    // updateNoteDirectory(["s2_1_1",5,"0-2-5",3,7],[7]);
+    // updateNoteDirectory([3,7],[3,"S1",1,"0-1",2]);
     // updateNoteData(["data1mod",1]);
-    // updatePassword(["123mod",1]);
-    // selectNote([3]);
+    // updatePassword(["123mod","donk"]);
+    // updateName(["cold hdk","donk"]);
     // selectAllnotes();
     // recentNotes(5);
-
+    // selectNote([3]);
+    // updataViewtime([1]);
+    // renameFolder(["CSE",5]);
+    // viewsubFolder(3);
+    // viewFolderContent(3);
 }
 
     // add
 	function addPerson(param){
 	    db.sql(`insert into Persons (PersonId, Name, Password) values(?, ?, ?)`,
-	        param).then((res)=>{
-	        console.log(res);
-	    }).catch((err)=>{
+	        param).catch((err)=>{
 	        console.log(err);
 	    });
 	}
 
     function addNote(param1,param2){
-        db.sql(`insert into Notes (NoteId, Title, data, Datetime) values(?, ?, ?, datetime('now','localtime'))`, param1).then((res)=>{
-                console.log(res);
-            }).catch((err)=>{
+        db.sql(`insert into Notes (NoteId, Title, data, FolderId, ModifyTime,ViewTime) values(?, ?, ?, ?, datetime('now','localtime'),datetime('now','localtime'))`, param1)
+        .catch((err)=>{
                 console.log(err);
             });
-        db.sql(`insert into Directories (id, Value, NoteId,ParentId,key,level) values(?,?,?,?,?,?)`, param2).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`insert OR ignore into Directories (FolderId, FolderName,ParentId,key,level) values(?,?,?,?,?)`, param2)
+        .catch((err)=>{
             console.log(err);
         });
     }
     
     function addSupport(param){
-        db.sql(`insert OR ignore into Support (Name, Content) values(?, ?) `,param).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`insert OR ignore into Support (Name, Content) values(?, ?) `,param).catch((err)=>{
             console.log(err);
         });
     }
     // delete
     function deletePerson(param){
-        db.sql(`delete from Persons where Personid = ?`, param).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`delete from Persons where Personid = ?`, param).catch((err)=>{
             console.log(err);
         });
     }
 
 	function deleteNote(param){
-	    db.sql(`delete from Notes where Noteid = ?`, param).then((res)=>{
-	        console.log(res);
-	    }).catch((err)=>{
+	    db.sql(`delete from Notes where Noteid = ?`, param).catch((err)=>{
 	        console.log(err);
 	    });
 	}
@@ -187,64 +179,98 @@ let doSql = function() {
 
     // update
     function updateNoteTitle(param){
-        db.sql(`update Notes set Title = ? ,Datetime = datetime('now','localtime') where NoteId = ?`, param).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`update Notes set Title = ? ,ModifyTime = datetime('now','localtime') where NoteId = ?`, param).catch((err)=>{
             console.log(err);
         });
     }
 
     function updateNoteDirectory(param1,param2){
-        db.sql(`update Directories set Value = ? ,ParentId= ?, key=?,level=? where NoteId = ?`, param1);
-        db.sql(`update Notes set Datetime = datetime('now','localtime') where NoteID = ?`,param2).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`update Notes set FolderId=?, ModifyTime = datetime('now','localtime') where NoteID = ?`,param1).catch((err)=>{
+            console.log(err);
+        });
+        db.sql(`insert OR ignore into Directories (FolderId, FolderName,ParentId,key,level) values(?,?,?,?,?)`, param2).catch((err)=>{
             console.log(err);
         });
     }
 
 	function updateNoteData(param){  
-	    db.sql(`update Notes set data= ? ,Datetime = datetime('now','localtime') where NoteId = ?`, param).then((res)=>{
-	        console.log(res);
-	    }).catch((err)=>{
+	    db.sql(`update Notes set data= ? ,ModifyTime = datetime('now','localtime') where NoteId = ?`, param).catch((err)=>{
 	        console.log(err);
 	    });
 	}
 
+    function updataViewtime(param){
+        db.sql(`update Notes set ViewTime= datetime('now','localtime') where NoteId = ?`, param).catch((err)=>{
+            console.log(err);
+        });
+    }
+
+    function updateName(param){
+        db.sql(`update Persons set Name = ? where PersonId = ?`, param).catch((err)=>{
+            console.log(err);
+        });
+    }
+
     function updatePassword(param){
-        db.sql(`update Persons set Password= ? where PersonId = ?`, param).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
+        db.sql(`update Persons set Password = ? where PersonId = ?`, param).catch((err)=>{
+            console.log(err);
+        });
+    }
+
+    function renameFolder(param){
+        db.sql(`update Directories set FolderName = ? where FolderId = ?`,param).catch((err)=>{
             console.log(err);
         });
     }
 
     // select
     function selectNote(param){
-        db.sql(`select * from Notes where NoteId = ?`, param,'get').then((res)=>{
-            console.log(res);
+        db.sql(`select NoteId,Title,data from Notes where NoteId = ?`, param,'get').then((res)=>{
+            var res_str = JSON.stringify(res);
+            console.log(res_str);
         }).catch((err)=>{
             console.log(err);
         });
     }
 
     function recentNotes(param){
-        db.sql(`select * from Notes ORDER BY Datetime DESC limit ?`,param,'all').then((res)=>{
-            console.log(res);
+        db.sql(`select NoteId,Title,data from Notes ORDER BY ModifyTime DESC limit ?`,param,'all').then((res)=>{
+            var res_str = JSON.stringify(res);
+            console.log(res_str);
         }).catch((err)=>{
             console.log(err);
         });
     }
 
     function selectAllnotes(param){
-        db.sql(`select * from Notes`,param,'all').then((res)=>{
-            console.log(res);
+        db.sql(`select NoteId,Title,data from Notes`,param,'all').then((res)=>{
+            var res_str = JSON.stringify(res);
+            console.log(res_str);
         }).catch((err)=>{
             console.log(err);
         });
     }
 
+    function viewsubFolder(param){
+        db.sql(`select FolderId,key,level from Directories where FolderId = ?`,param,'get').then((res)=>{
+            var pattern = res.key+'-'+res.FolderId;
+            db.sql(`select FolderId,FolderName from Directories where key LIKE ?`,pattern,'all').then((res)=>{
+                console.log(res);
+            })
+        }).catch((err)=>{
+            console.log(err);
+        });
 
+    }
+ 
+    function viewFolderContent(param){
+        db.sql(`select NoteId,Title,data from Notes where FolderId = ?`,param,'all').then((res)=>{
+            var res_str = JSON.stringify(res);
+            console.log(res_str);
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
 
 
 
